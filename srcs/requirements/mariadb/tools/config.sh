6d01fe04c999f022a37c5c68e33e3eeebe99381c
mqueguin <mqueguin@student.42.fr>
2022-10-24 10:43:17 -0400

    Ajout de adminer, ftp et redis (redis marche pas a cause des droits wordpress)

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-#if [ ! -d "/var/run/mysqld" ]; then
 mkdir -p /var/run/mysqld
 chown -R mysql:mysql /var/run/mysqld
-#fi
 
-
-#if [ ! -d "/var/lib/mysql/mysql" ]; then
 chown -R mysql:mysql /var/lib/mysql
 mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql
 
@@ -17,7 +13,6 @@ sed  -i 's/$MYSQL_PASSWORD/'$MYSQL_PASSWORD'/g' /tmp/init_sql.sql
 sed  -i 's/$MYSQL_ROOT_PWD/'$MYSQL_ROOT_PWD'/g' /tmp/init_sql.sql
 
 /usr/sbin/mysqld --user=mysql --bootstrap < /tmp/init_sql.sql
-#fi
 
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
